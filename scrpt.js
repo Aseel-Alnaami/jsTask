d8114a53f56cb0aaef9c519c2679735c4b9b71c4
Aseel-Alnaami <aseelalnaami1@gmail.com>
2024-12-12 15:02:41 +0300

    first commit

---
+++ scrpt.js
@@ -0,0 +1,20 @@
+let username = prompt("Enter your username:");
+document.getElementById("welcome-message").textContent = `Welcome, ${username}!`;
+
+let password;
+let attempts = 5;
+
+while (attempts > 0) {
+    password = prompt("Enter your password:");
+    if (password === "123") {
+        alert(`Welcome, ${username}!`);
+        break;
+    } else {
+        attempts--;
+        if (attempts > 0) {
+            alert(`Incorrect password. You have ${attempts} attempts left.`);
+        } else {
+            alert("Please try again later.");
+        }
+    }
+}
